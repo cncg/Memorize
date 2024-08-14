4de4643efe73e6a48ce09ec120a3a1311cae4fc4
Chelzo <ccalvo0314@gmail.com>
2024-08-14 12:38:11 -0400

    initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Chelsea Calvo on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
