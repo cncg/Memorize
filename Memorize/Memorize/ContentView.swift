3b155ce91b18ac1fe6b00c16ec4c63feb78d3e65
Chelzo <ccalvo0314@gmail.com>
2024-10-03 13:59:27 -0400

    reconnecting repo

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -3,20 +3,11 @@
 //  Stanford Memorize App
 //
 // NOTES TO TRANSFER TO NOTEBOOK 9/11/2024 - DAY THREE
-//- What is trailing closure syntax?
-//- What is trailing and leading?
-//- What kinds of logic can Views not do?
 //- What are structs ? What are considered structs?
-//  Make sure to use things in the ViewBuilder witht he right let or var, if things are going to change, use var. Use let and then see what needs to be changed or remember . The isFaceUp needs to be changed and not let for example. This value will change and is not constant.
+//  Make sure to use things in the ViewBuilder with the right let or var, if things are going to change, use var. Use let and then see what needs to be changed or remember . The isFaceUp needs to be changed and not let for example. This value will change and is not constant.
 //- What is Type inference? Once a type is set it cannot be changed later! Big change from Javascript.
 //  OPTION _ CLICK = what does this do? show types?
-//  What is @Sate ? Is this permanent or temporary?
-//-  Views are Immutable . What does that mean?
-//- Arrays in swift, how to make them ? setting a type?
-//  ARRAYS START AT 0! index number 0 is the start, the number one, the first.
-//- What is alternate array notation?
-//  PRACTICE DRY. DONT REPEAT YOURSELF. REPEATED CODE? FIX IT !
-//- What is ForEach? And how does the RANGE of integers or sequence work? Is this a tradional for loop?
+//  What is @Sate ? Is this permanent or temp?
 //- What does indicies mean?
 //- What is the base thing we used in the ZStack?
 //  GET USED TO ARRAYS !
@@ -27,10 +18,14 @@
 //- Internal vs External parameter names
 //- What is opacity ? Why change the if else in ViewBuilder to do opacity instead. Changing the isFaceUp form an if else is because when using the if statement, if the card was face down, it would completly change the state of thhe card and there would be no emoji in the content anymore; shrinking the card to nothing. when using opacity you can still have the front or back of the card with emoji in tact it will jsut be invisible when toggled, making sure nothing is shrinked.
 //- What is Group?
-//
+//- What is a LazyVGrid?
+//- How can I know when to add logic or a function?
 
 
 import SwiftUI
+
+
+// now fix the toggle, you fucked the code up you had and what you did is gone! great work ! now we are using the code the professor used. you didnt have the adder and remvoer at te bottom of the screen and the cards didnt have an aspect ratio. now oyu need to add the card toggle, to isfaceup. fix it
 
 struct ContentView: View {
     let emojis = ["👻","🎃","🕷️","⚰️","🧟‍♂️","🍬", "🍭", "💀", "🧛‍♂️", "🧙‍♀️", "🕸️", "👽"]
@@ -38,82 +33,34 @@ struct ContentView: View {
     @State var cardCount: Int = 4
     
     var body: some View {
-        VStack {
-            ScrollView {
-                cards
-            }
+        ScrollView {
+            cards
             
-            Spacer()
-            cardCountAdjusters
         }
         .padding()
     }
     
-    var cards : some View {
-        LazyVGrid(columns:[GridItem(.adaptive(minimum:120))]) {
-            ForEach(0..<cardCount,id: \.self) { index in        // need more learning
+    var cards: some View {
+        LazyVGrid(columns:[GridItem(.adaptive(minimum:125))]) {
+            ForEach(emojis.indices, id: \.self) { index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        
-    }
-    
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    
-    var cardRemover : some View {
-        return cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder : some View {
-        return cardCountAdjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
     }
 }
 
 struct CardView: View {
-    let content : String
-    
-    @State var isFaceUp = true
-    
+    let content: String
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: 3)
-                Text(content)
-                    .font(.largeTitle)
-            }
-            .opacity(isFaceUp ? 1 : 0)
-            base.fill().opacity(isFaceUp ? 0 : 1)
-            
+            base.strokeBorder(lineWidth: 2)
+            Text(content).font(.largeTitle)
         }
-        .onTapGesture {
-            isFaceUp.toggle()
-        }
-        
     }
 }
-
-
 
 #Preview {
     ContentView()
---
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Chelsea Calvo on 9/27/24.
+//
+//- Why use a class instead of a struct here?
+//
+//
+
+import SwiftUI
+
+//class EmojiMemoryGame {
+//    var model: MemoryGame<String>
+//}
